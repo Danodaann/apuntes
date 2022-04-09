9c6f789308552bb4e56c3506602840d85b025e99
Daniel Alejandro <danodaannn@gmail.com>
2022-04-09 15:20:45 -0500

    first commit

---
+++ JS/controlDeFlujo.js
@@ -0,0 +1,18 @@
+// IF 
+if (condition) {
+    document.write("Esto se va a ejecutar si la condicion se cumple True")
+}
+
+// WHILE 
+// mientras la condicion sea True se ejecuta, se deja de ajecutar hasta que la condicion sea False
+let x = 0;
+while (condition) {
+    document.write(x)
+    x++;
+}
+// SWITCH 
+// Para diferentes casos, es importante el break;
+
+
+
+//FOR 
